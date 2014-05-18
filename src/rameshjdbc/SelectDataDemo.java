db0ccdfc8c11d2c507822354a2b559196ed086af
ramesh590 <rramesh@lingualspeech.com>
2014-05-18 19:14:58 +0530

    insertstmt not using any where

--- src/rameshjdbc/SelectDataDemo.java
+++ src/rameshjdbc/SelectDataDemo.java
@@ -1,43 +1,43 @@
 package rameshjdbc;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
- 
-public class SelectDataDemo {
-    public static void main(String[] args) {
-        Connection connection = null;
-        Statement insertStmt = null;
-        Statement selectStmt = null;
-        try
-        {
-            Class.forName("com.mysql.jdbc.Driver");
-            connection = DriverManager.getConnection("jdbc:mysql:localhost:3306/lingual", "root", "  mysql");
-             
-            /*insertStmt = connection.createStatement();
-            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (1,'Lokesh','Gupta',5)");
-            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (2,'howtodoinjava','com',5)");*/
-             
-            selectStmt = connection.createStatement();
-            ResultSet rs = selectStmt.executeQuery("SELECT CID,CNAME FROM CUSTOMER WHERE CID = 100");
-            while(rs.next())
-            {
-                System.out.println(rs.getInt(1));    //First Column
-                System.out.println(rs.getString(2));    //Second Column
-                
-            }
-        }
-        catch (Exception e) {
-            e.printStackTrace();
-        }finally {
-            try {
-                selectStmt.close();
-                insertStmt.close();
-                connection.close();
-            } catch (Exception e) {
+	import java.sql.Connection;
+	import java.sql.DriverManager;
+	import java.sql.ResultSet;
+	import java.sql.Statement;
+	 
+	public class SelectDataDemo {
+	    public static void main(String[] args) {
+	        Connection connection = null;
+	        //Statement insertStmt = null;
+	        Statement selectStmt = null;
+	        try
+	        {
+	            Class.forName("com.mysql.jdbc.Driver");
+	            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/LINGUAL", "root", "mysql");
+	             
+	            /*insertStmt = connection.createStatement();
+	            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (1,'Lokesh','Gupta',5)");
+	            insertStmt.execute("INSERT INTO EMPLOYEE (ID,FIRST_NAME,LAST_NAME,STAT_CD) VALUES (2,'howtodoinjava','com',5)");*/
+	             
+	            selectStmt = connection.createStatement();
+	            ResultSet rs = selectStmt.executeQuery("SELECT CID,CNAME FROM CUSTOMER WHERE CID = 102");
+	            while(rs.next())
+	            {
+               System.out.println(rs.getInt("CID"));    //First Column
+	                System.out.println(rs.getString("CNAME"));    //Second Column
+	                
+	            }
+	        }
+	        catch (Exception e) {
+	            e.printStackTrace();
+	        }finally {
+	            try {
+	                selectStmt.close();
+	         //       insertStmt.close();
+	                connection.close();
+	            } catch (Exception e) {
                 e.printStackTrace();
-            }
-        }
-    }
-}
+	            }
+	        }
+	    }
+	}
